94ff2e2952fbcaa0d6cf9f599ad0200c6e67a1b7
Nikolay Vinogradskiy <vinogradskiy.n@gmail.com>
2016-08-08 10:54:10 +0200

    -

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import {GOOGLE_MAPS_PROVIDERS} from 'angular2-google-maps/core';
+import {disableDeprecatedForms, provideForms} from '@angular/forms'; 
 import { AppComponent, environment } from './app/';
 
 if (environment.production) {
@@ -8,5 +9,7 @@ if (environment.production) {
 }
 
 bootstrap(AppComponent, [
+  disableDeprecatedForms(),
+  provideForms(),
   GOOGLE_MAPS_PROVIDERS
 ]);
