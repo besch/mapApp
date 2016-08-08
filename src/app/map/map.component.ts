6dbef0c9ebc21ed390462ece457b27eeb547fa4d
Nikolay Vinogradskiy <vinogradskiy.n@gmail.com>
2016-08-08 10:38:19 +0200

    added maps, material2

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,8 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, provide, OnInit } from '@angular/core';
+import {bootstrap} from '@angular/platform-browser-dynamic';
+
+import {
+  MapsAPILoader,
+  NoOpMapsAPILoader,
+  MouseEvent,
+  GOOGLE_MAPS_PROVIDERS,
+  GOOGLE_MAPS_DIRECTIVES
+} from 'angular2-google-maps/core';
+
+import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
+import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
 
 @Component({
   moduleId: module.id,
   selector: 'app-map',
+  directives: [GOOGLE_MAPS_DIRECTIVES, MD_CARD_DIRECTIVES, MD_BUTTON_DIRECTIVES],
   templateUrl: 'map.component.html',
   styleUrls: ['map.component.less']
 })
