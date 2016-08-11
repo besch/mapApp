855faba067d447de373052e91f218b23760369d6
Nikolay Vinogradskiy <vinogradskiy.n@gmail.com>
2016-08-11 17:04:32 +0200

    Setting up map component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MapComponent } from './map/index';
+// import { MapComponent } from './map/map.component';
 
 @Component({
   moduleId: module.id,
