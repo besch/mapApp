c25a2adf87439df4b06366f818b515c93539400e
Nikolay Vinogradskiy <vinogradskiy.n@gmail.com>
2016-08-11 13:33:31 +0200

    Google maps lib setup completed

--- src/system-config.ts
+++ src/system-config.ts
@@ -9,7 +9,8 @@
  **********************************************************************************************/
 /** Map relative paths to URLs. */
 const map: any = {
-  '@angular2-material': 'vendor/@angular2-material'
+  '@angular2-material': 'vendor/@angular2-material',
+  'angular2-google-maps': 'vendor/angular2-google-maps'
 };
 
 /** User packages configuration. */
